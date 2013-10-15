f25f7a5c5122ec6d783bc5df99db58047e98a196
Cori Allen <coriallen@gmail.com>
2013-10-15 17:24:24 -0400

    gets actions working.

--- dream_tracker/app/controllers/dreams_controller.rb
+++ dream_tracker/app/controllers/dreams_controller.rb
@@ -1,25 +1,45 @@
 class DreamsController <ApplicationController
-	before_action :set_dream, only [:show, :edit, :update, :destroy]
+	before_action :set_dream, only: [:show, :edit, :update, :destroy]
 
 	def show
 	end
+
 	def index
 		@dreams = Dream.all
 	end
+
 	def create
+		@dream = Dream.new(dream_params)
+		if @dream.save
+			redirect_to @dream
+		else
+			render action: 'new'
+		end
 	end
+
 	def new
 		@dream = Dream.new
 	end
+
 	def update
+		if @dream.update(dream_params)
+			redirect_to @dream
+		else
+			render action: 'edit'
+		end
 	end
 	def edit
 	end
+
 	def destroy
+		@dream.destroy
+		redirect_to dream_url
 	end
 	private
 	def set_dream
+		@dream = Dream.find(params[:id])
 	end
 	def dream_params
+		params.require(:dream).permit(:subject, :good_dream, :recurring_dream, :description, :emotions, :vividness, :image_url)
 	end
 end
